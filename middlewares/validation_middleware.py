bae1ad3113000d746a55ac1600736da450730051
afaqrehman98 <afaqrehman98@gmail.com>
2024-07-07 23:29:30 +0200

    updated middlware to handle empty query parameters

--- middlewares/validation_middleware.py
+++ middlewares/validation_middleware.py
@@ -6,6 +6,16 @@ def validate_destination(func):
     def wrapper(*args, **kwargs):
         destination = request.args.get('destination')
         if destination and len(destination) != 3:
-            return jsonify({'error': 'Destination must be a 3-character airport code'}), 400
+            return jsonify({'error': 'Destination airport code must be exactly 3 characters long'}), 400
+        return func(*args, **kwargs)
+    return wrapper
+
+def validate_parameters(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        destination = request.args.get('destination')
+        airlines = request.args.getlist('airlines')
+        if not destination and not airlines:
+            return jsonify({'error': 'Either destination or airlines should be provided to fetch the data'}), 400
         return func(*args, **kwargs)
     return wrapper
--- views/flight_view.py
+++ views/flight_view.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, jsonify, request
 from controllers.flight_controller import get_flight_info_controller
-from middlewares.validation_middleware import validate_destination
+from middlewares.validation_middleware import validate_parameters, validate_destination
 
 flight_bp = Blueprint('flight_bp', __name__)
 
 @flight_bp.route('/get_flight_info', methods=['GET'])
 @validate_destination
+@validate_parameters
 def get_flight_info():
     destination = request.args.get('destination')
-    airlines = request.args.getlist('airlines')  # Get list of airlines or single value
+    airlines = request.args.getlist('airlines')
     flights_info = get_flight_info_controller(destination, airlines)
     return jsonify(flights_info)
