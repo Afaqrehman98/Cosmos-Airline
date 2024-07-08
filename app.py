bbe4420c828806be7d7ffc46f8bc1a4b3905e00d
afaqrehman98 <afaqrehman98@gmail.com>
2024-07-08 03:11:30 +0200

    Handle sensitive case query issue

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask,render_template
 from views.flight_view import flight_bp
-
+from flask_cors import CORS
 app = Flask(__name__)
 
-
+CORS(app)
 # Register blueprints
 app.register_blueprint(flight_bp)
 
--- views/flight_view.py
+++ views/flight_view.py
@@ -8,7 +8,7 @@ flight_bp = Blueprint('flight_bp', __name__)
 @validate_destination
 @validate_parameters
 def get_flight_info():
-    destination = request.args.get('destination')
-    airlines = request.args.getlist('airlines')
+    destination = request.args.get('destination', '').upper()  # Normalize to uppercase
+    airlines = [airline.upper() for airline in request.args.getlist('airlines')] 
     flights_info = get_flight_info_controller(destination, airlines)
     return jsonify(flights_info)
